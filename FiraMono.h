4a51e47da38582c30991a2e873af147d3b3da2c8
Matt Jenkins <matt@majenko.co.uk>
2017-09-03 00:35:47 +0100

    Initial import

---
+++ FiraMono.h
@@ -0,0 +1,29 @@
+#ifndef _FIRAMONO_H
+#define _FIRAMONO_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t FiraMono10[];
+    extern const uint8_t FiraMono11[];
+    extern const uint8_t FiraMono12[];
+    extern const uint8_t FiraMono13[];
+    extern const uint8_t FiraMono14[];
+    extern const uint8_t FiraMono15[];
+    extern const uint8_t FiraMono16[];
+    extern const uint8_t FiraMono17[];
+    extern const uint8_t FiraMono18[];
+    extern const uint8_t FiraMono19[];
+    extern const uint8_t FiraMono20[];
+    extern const uint8_t FiraMono21[];
+    extern const uint8_t FiraMono22[];
+    extern const uint8_t FiraMono23[];
+    extern const uint8_t FiraMono24[];
+    extern const uint8_t FiraMono25[];
+    extern const uint8_t FiraMono26[];
+    extern const uint8_t FiraMono27[];
+    extern const uint8_t FiraMono28[];
+    extern const uint8_t FiraMono29[];
+    extern const uint8_t FiraMono30[];
+};
+#endif
